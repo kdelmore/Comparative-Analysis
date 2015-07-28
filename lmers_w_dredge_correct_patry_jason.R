fa821cc0b0bb7dd28f5ef65b7bfb786f18959f9b
Kira <kdelmore@zoology.ubc.ca>
2015-07-28 13:19:59 -0700

    more additions

--- lmers_w_dredge_correct_patry_jason.R
+++ lmers_w_dredge_correct_patry_jason.R
@@ -24,15 +24,39 @@ comp$sp1_genus<-as.factor(comp$sp1_genus)
 comp$migration_category<-as.factor(comp$migration_category)
 #comp$visual_official<-as.factor(comp$visual_official)
 #comp$visual_official_ordered<-ordered(comp$visual_official)
-comp$visual_official<-as.factor(comp$visual_official_jason)
+comp$visual_official<-as.factor(comp$visual_official)
 comp$visual_official_ordered<-ordered(comp$visual_official)
+comp$visual_official_jason<-as.factor(comp$visual_official_jason)
+comp$visual_official_ordered_jason<-ordered(comp$visual_official_jason)
+comp$visual_official_kira_jason<-as.factor(comp$visual_official_kira_jason)
+comp$visual_official_ordered_kira_jason<-ordered(comp$visual_official_kira_jason)
 #comp$visual_official_2<-as.factor(comp$visual_official_2)
 #comp$allo_sym<-as.factor(comp$allo_sym)
 #comp$allo_sym_para_perp<-as.factor(comp$allo_sym_para_perp)
 
-#### combined, visual then overlap
+#### combined, visual
 
 Global_Personal.Use<-lmer(avg_log ~migration_category+visual_official_ordered+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus) ,data=comp,na.action = na.pass)
+All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
+Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
+Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
+Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
+
+Global_Personal.Use<-lmer(avg_log ~migration_category+visual_official_ordered_jason+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus) ,data=comp,na.action = na.pass)
+All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
+Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
+Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
+Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
+
+Global_Personal.Use<-lmer(avg_log ~migration_category+visual_official_ordered_kira_jason+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus) ,data=comp,na.action = na.pass)
+All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
+Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
+Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
+Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
+
+#### combined, overlap
+
+Global_Personal.Use<-lmer(avg_log ~migration_category+overlap_proportion_log+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus),data=comp,na.action = na.pass)
 All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
 Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
 Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
@@ -43,63 +67,3 @@ final<-lmer(avg_log ~migration_category+breed_lat_avg_log+p_distance_log+mass_av
 pdf("visreg_migration.pdf",width=4,height=4)
 visreg(final,"migration_category",xlab="migration category", line=c(lwd=2), points=list(col="black",cex=5))
 dev.off()
-
-Global_Personal.Use<-lmer(avg_log ~migration_category+overlap_proportion_log+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus),data=comp,na.action = na.pass)
-All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
-Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
-Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
-Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
-
-#### colour, visual then overlap
-
-Global_Personal.Use<-lmer(avg_colour_log ~migration_category+visual_official_ordered+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus),data=comp,na.action = na.pass)
-All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
-Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
-Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
-Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
-
-Global_Personal.Use<-lmer(avg_colour_log ~migration_category+overlap_proportion_log+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus),data=comp,na.action = na.pass)
-All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
-Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
-Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
-Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
-
-#### song, visual then overlap
-
-Global_Personal.Use<-lmer(avg_song_log ~migration_category+visual_official_ordered+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus),data=comp,na.action = na.pass)
-All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
-Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
-Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
-Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
-
-Global_Personal.Use<-lmer(avg_song_log ~migration_category+overlap_proportion_log+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus),data=comp,na.action = na.pass)
-All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
-Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
-Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
-Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
-
-#### morph, visual then overlap
-
-Global_Personal.Use<-lmer(avg_morph_log ~migration_category+visual_official_ordered+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus),data=comp,na.action = na.pass)
-All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
-Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
-Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
-Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
-
-Global_Personal.Use<-lmer(avg_morph_log ~migration_category+overlap_proportion_log+breed_lat_avg_log+distance_avg_log+p_distance_log+mass_avg_log + (1 | family/sp1_genus),data=comp,na.action = na.pass)
-All_models_Personal.Use<-dredge(Global_Personal.Use, extra = "R^2") # all possible combinations of fixed effects
-Sub_All_models_Personal.Use<-subset(All_models_Personal.Use, delta<2) # top models (less than delta AICc = 2 from 'best' model)
-Avg.Personal.Use<-model.avg(All_models_Personal.Use, subset=delta<2) # model averaging
-Avg.Personal.Use$coefTable # parameter estimates and SEs from average model
-
-## plot two cats
-as.factor(comp$allo_sym_para_perp)
-palette(value=c("black","black","red","red"))
-pch_lookup <- c(parallel_allopatric = 1, parallel_sympatric = 16, perpendicular_allopatric = 1, perpendicular_sympatric = 16)
-plot(comp$p_distance,comp$avg_2,col=comp$allo_sym_para_perp,pch=pch_lookup[as.numeric(comp$allo_sym_para_perp)],xlab=("time since divergence"),ylab=("divergence"))
-
-## plot three cats
-as.factor(comp$visual_official_2)
-palette(value=c("black","black","black","red","red","red"))
-pch_lookup <- c(parallel_allo = 2, parallel_sym = 16, parallel_para = 1, perpendicular_allo = 2, perpendicular_sym = 16, perpendicular_para = 1)
-plot(comp$p_distance,comp$avg_2,col=comp$visual_official_2,pch=pch_lookup[as.numeric(comp$visual_official_2)],xlab=("time since divergence"),ylab=("divergence"))
